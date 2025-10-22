3677ce1725f2cf5baacf0ea1bbda8b3949295c6a
SetAlf <s3lfan0@gmail.com>
2025-10-22 17:37:46 -0500

    changed login layout and added login page; reroute link to login page

---
+++ client/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react';
+import Login from '@react-login-page/page4';
+
+const Demo = () => (
+  <Login style={{ height: 600 }}>
+    <Login.Password>
+    </Login.Password>
+  </Login>
+);
+
+export default Demo;
