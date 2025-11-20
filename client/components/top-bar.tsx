a8a38f093ff2f789b22bb0617a436e53edc745c2
huston <hustonw70@gmail.com>
2025-11-20 17:05:32 -0600

    commented out useless buttons

--- client/components/top-bar.tsx
+++ client/components/top-bar.tsx
@@ -24,7 +24,7 @@ const roleConfig = {
     tabs: [
       { label: "Appointments", href: "/patient/appointments" },
       { label: "Files", href: "/patient/files" },
-      { label: "Other", href: "/patient/other" },
+      
     ],
   },
   doctor: {
@@ -33,7 +33,7 @@ const roleConfig = {
       { label: "Appointments", href: "/doctor/appointments" },
       { label: "Patients", href: "/doctor/patients" },
       { label: "Medical Records", href: "/doctor/medical-records" },
-      { label: "Other", href: "/doctor/other" },
+      
     ],
   },
   staff: {
@@ -42,7 +42,7 @@ const roleConfig = {
       { label: "Agenda", href: "/staff/appointments" },
       { label: "Patients", href: "/staff/patients" },
       { label: "Doctors", href: "/staff/doctors" },
-      { label: "Other", href: "/staff/other" },
+      
     ],
   },
 }
